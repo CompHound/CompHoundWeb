ebfe801011a75bf22d686722790e6ec3e7ab1291
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-09-09 23:14:13 +0200

    implemented update2

--- routes.js
+++ routes.js
@@ -3,7 +3,8 @@ module.exports = function(app) {
   app.get('/api/v1/instances', instances.findAll);
   app.get('/api/v1/instances/:id', instances.findById);
   app.post('/api/v1/instances', instances.add);
-  app.put('/api/v1/instances/:id', instances.update);
+  //app.put('/api/v1/instances/:id', instances.update); // this one does not allow me to PUT a new instance only update existing
+  app.put('/api/v1/instances/:id', instances.update2); // works more like POST + PUT
   app.delete('/api/v1/instances/:id', instances.delete);
   app.get('/api/v1/instances/project/:pid', instances.findAllForProject);
 }
