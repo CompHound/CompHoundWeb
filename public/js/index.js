b5f2b634c81ce04332dd94677cec16c72848ad27
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-09-22 16:36:14 +0200

    implemented event to report on user picking datatable row

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,20 @@ $(document).ready(function() {
   });
   //.columnFilter();
 });
+
+// Support for selecting a row.
+// http://stackoverflow.com/questions/3458571/jquery-click-event-on-tr-elements-with-in-a-table-and-getting-td-element-v
+
+$(document).ready(function() {
+  $("div#datatable_wrapper > table#datatable > tbody > tr").live('click', function() {
+    alert("You clicked my <tr>!");
+    // get <td> element values
+  });
+});
+
+$(document).ready(function() {
+  $("div#datatable_wrapper > table#datatable > tbody > tr > td").live('click', function() {
+     alert("You clicked my <td>: " + $(this).html()
+      + "... My TR is: " + $(this).parent("tr").html());
+  });
+});
