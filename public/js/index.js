911a315727270d44d46a78c646282b9bf7c240be
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-10-25 16:15:16 +0100

    added isolate button, but guid is not found in mapping

--- public/js/index.js
+++ public/js/index.js
@@ -41,61 +41,66 @@ $(document).ready(function() {
 
 var lmv_initialised = false;
 var viewer = null;
+var urn = id = '';
 
-$(document).ready(function() {
+$(document).ready(
+  function() {
+    $("div#datatable_wrapper > table#datatable > tbody > tr > td").live('click',
+      function() {
 
-  $("div#datatable_wrapper > table#datatable > tbody > tr > td")
-    .live('click', function() {
+        var tr = $(this).parent("tr");
+        var n = tr.find("td").length;
 
-      var tr = $(this).parent("tr");
-      var n = tr.find("td").length;
+        //console.log(
+        //  "You clicked TD: " + $(this).html()
+        //  + "... its TR is: " + tr.html()
+        //  + " and has " + n.toString() + " children");
 
-      //console.log(
-      //  "You clicked TD: " + $(this).html()
-      //  + "... its TR is: " + tr.html()
-      //  + " and has " + n.toString() + " children");
+        //var i = 0;
+        //data = {};
+        //tr.find("td").each(function() {
+        //  data[columnNames[i++]] = $(this).text();
+        //});
 
-      //var i = 0;
-      //data = {};
-      //tr.find("td").each(function() {
-      //  data[columnNames[i++]] = $(this).text();
-      //});
+        var p = $("p#instance");
+        p.text('Selected component occurrence:');
+        p = p.next("p");
+        if(p) { p.remove(); }
+        var table = $("table#instance");
+        table.empty();
+        table.append('<colgroup><col class="twocolumn"><col class="twocolumn"></colgroup>');
+        var i = 0;
+        tr.find("td").each(function() {
+          var colname = columnNames[i++];
+          var coltext = $(this).text();
+          if( colname == 'urn') { urn = coltext; }
+          else if( colname == 'id') { id = coltext; }
 
-      var p = $("p#instance");
-      p.text('Selected component occurrence:');
-      p = p.next("p");
-      if(p) { p.remove(); }
-      var urn = id = '';
-      var table = $("table#instance");
-      table.empty();
-      table.append('<colgroup><col class="twocolumn"><col class="twocolumn"></colgroup>');
-      var i = 0;
-      tr.find("td").each(function() {
-        var colname = columnNames[i++];
-        var coltext = $(this).text();
-        if( colname == 'urn') { urn = coltext; }
-        else if( colname == 'id') { id = coltext; }
+          table.append('<tr><td>'
+            + capitalize(colname) + '</td><td>'
+            + coltext + '</td></tr>' );
+        });
 
-        table.append('<tr><td>'
-          + capitalize(colname) + '</td><td>'
-          + coltext + '</td></tr>' );
-      });
+        // Temporarily hardcoded urn and element id.
 
-      // Temporarily hardcoded urn and element id.
+        var urn_rst_advanced_sample_project = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Y29tcGhvdW5kLWJ1Y2tldC9yc3RfYWR2YW5jZWRfc2FtcGxlX3Byb2plY3QucnZ0';
+        if( !urn ) { urn = urn_rst_advanced_sample_project; }
+        if( !id ) { id = '4b50b624-3213-4043-a30c-ac2eba74ca69-00028301'; }
 
-      var urn_rst_advanced_sample_project = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Y29tcGhvdW5kLWJ1Y2tldC9yc3RfYWR2YW5jZWRfc2FtcGxlX3Byb2plY3QucnZ0';
-      if( !urn ) { urn = urn_rst_advanced_sample_project; }
-      if( !id ) { id = '4b50b624-3213-4043-a30c-ac2eba74ca69-00028301'; }
-
-      if( !lmv_initialised && urn && id ) {
-        $("p#viewer").remove();
-        viewer = lmv_initialize( urn, id );
-        lmv_initialised = true;
+        if( !lmv_initialised && urn && id ) {
+          $("p#viewer").remove();
+          viewer = lmv_initialize( urn, id );
+          lmv_initialised = true;
+        }
       }
+    );
+  }
+);
 
-      //if( lmv_initialised && viewer && urn && id ) {
-      //  var nodeid = getNodeIdByGuid (id);
-      //  viewer.isolate(nodeid);
-      //}
-    });
-});
+function isolate() {
+  alert('Isolate.');
+  if( lmv_initialised && viewer && urn && id ) {
+    var nodeid = getNodeIdByGuid(id);
+    viewer.isolate(nodeid);
+  }
+}
